6bccc1c62f5a16d8daf87c1bc1486e33589e3c58
JoseWRPereira <josewrpereira@tutanota.com>
2022-11-23 22:06:12 -0300

    last session test

--- app/app.py
+++ app/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
-from flask import jsonify, json
+from flask import jsonify
 from flask import render_template, redirect, url_for, request
 from flask import session
 
 app = Flask(__name__)
 app.secret_key = 'k1k2'
 
-class Saida():
+
+class Saidas():
     def __init__(self):
         self.k1 = 0
         self.k2 = 0
@@ -19,25 +20,21 @@ class Saida():
     def set_k2(self, valor):
         self.k2 = valor
 
-saida = Saida()
+saida = Saidas()
 
 @app.route("/get")
 def get():
-    # saida.k1 = session['k1']
-    # saida.k2 = session['k2']
     return jsonify([{"k1":saida.get_k1(), "k2":saida.get_k2()}])
 
+
+
+
+
 @app.route("/")
-def init():
-    # session['k1'] = 0
-    # session['k2'] = 0
-    return redirect(url_for('index'))
-
-
 @app.route("/index")
 def index():
-    # saida.k1 = session['k1']
-    # saida.k2 = session['k2']
+    saida.set_k1(session['k1'])
+    saida.set_k2(session['k2'])
     print("K1 = {};  K2 = {};".format(saida.get_k1(),saida.get_k2()) )
     return render_template("index.html", k_1=saida.get_k1(), k_2=saida.get_k2())
 
@@ -46,24 +43,20 @@ def index():
 def saidas():
     t1 = request.args.get('K1')
     t2 = request.args.get('K2')
+           
 
     if t1==None: 
         saida.set_k1(0)
     else:
         saida.set_k1(1)
-    # session['k1'] = saida.k1
 
     if t2==None:
         saida.set_k2(0)
     else:
         saida.set_k2(1)
-    # session['k2'] = saida.k2
+
+    session['k1'] = saida.get_k1()
+    session['k2'] = saida.get_k2()
 
     print("K1 = {};  K2 = {};".format(saida.get_k1(),saida.get_k2()) )
     return redirect(url_for('index'))
-
-    # session.pop('k1', None)
-
-
-# if __name__=="__main__":
-#     app.run()
